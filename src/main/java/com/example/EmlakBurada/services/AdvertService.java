3130742a4bfe36b995ea7502289d0f9cfd005480
zeynepaltay <altay-zeynep@hotmail.com>
2025-06-23 16:23:02 +0300

    general service fixation

--- src/main/java/com/example/EmlakBurada/services/AdvertService.java
+++ src/main/java/com/example/EmlakBurada/services/AdvertService.java
@@ -1,97 +1,25 @@
 package com.example.EmlakBurada.services;
 
-import com.example.EmlakBurada.models.ImageData;
-import com.example.EmlakBurada.models.dtos.request.AdvertSaveRequest;
-import com.example.EmlakBurada.converter.AdvertConverter;
-import lombok.RequiredArgsConstructor;
 import com.example.EmlakBurada.models.Adverts;
 import com.example.EmlakBurada.models.Enums.AdvertStatus;
-import com.example.EmlakBurada.models.Users;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.EmlakBurada.models.dtos.request.AdvertSaveRequest;
 import org.springframework.stereotype.Service;
-import com.example.EmlakBurada.repositories.AdvertRepository;
-
 import java.util.List;
-import java.util.Optional;
-@RequiredArgsConstructor
+
 @Service
-public class AdvertService {
-    private final ImageDataService imageDataService;
-    AdvertRepository advertRepository;
-    private final UserService userService;
-    @Autowired
-    public AdvertService(AdvertRepository advertRepository, UserService userService, ImageDataService imageDataService) {
-        this.advertRepository = advertRepository;
-        this.userService= userService;
-        this.imageDataService = imageDataService;
-    }
-    public List<Adverts> getAllAdverts(){
-        List<Adverts> result = advertRepository.findAll();
-        return result;
-    }
-    public Adverts createAdvert(AdvertSaveRequest adverts) {
-        Users users=null;
-        Users advertiser=null;
-        List<ImageData> imageData= null;
-        if(adverts.getUserId() !=null){
-            users=userService.getUser(adverts.getUserId());
-        }
-        if(adverts.getAdvertiserId() !=null){
-            advertiser=userService.getUser(adverts.getAdvertiserId());
-        }
-        if(adverts.getImageId() !=null){
-            imageData = imageDataService.findById(adverts.getImageId());
-        }
-        Adverts adverts1 = AdvertConverter.toAdvert(adverts,users,advertiser);
-        return advertRepository.save(adverts1);
-    }
+public interface AdvertService {
 
-    public Adverts updateAdvert(AdvertSaveRequest adverts) {
-        Users users = null;
-        Users advertisers = null;
-        List<ImageData> imageData = null;
-        if(adverts.getUserId() != null){
-            users = userService.getUser(adverts.getUserId());
-        }
-        if(adverts.getAdvertiserId() !=null){
-            advertisers = userService.getUser(adverts.getAdvertiserId());
-        }
-        if(adverts.getImageId() !=null){
-            imageData = imageDataService.findById(adverts.getImageId());
-        }
-        // Advert nesnesini oluşturun
-        Adverts adverts1 = AdvertConverter.toAdvert(adverts,users,advertisers);
+    List<Adverts> getAllAdverts();
 
-        // Veritabanına kaydedin
-        return advertRepository.save(adverts1);
-//        Adverts adverts1 = AdvertConverter.toAdvert(adverts);
-//        Users user=adverts.getUserId();
-//        advertRepository.save(adverts1);
-    }
+    Adverts createAdvert(AdvertSaveRequest adverts);
 
-    public boolean deleteAdvert(Long id) {
-        Optional<Adverts> advert = advertRepository.findById(id);
-        if (advert.isPresent()) {
-            advertRepository.deleteById(id);
-            return true;
-        } else {
-            return false;
-        }//advertRepository.deleteById(id);
-    }
-    public Adverts getAdvert(Long id) {
-        return advertRepository.findById(id).get();
-    }
-    public List<Adverts> filterByAdvert(AdvertStatus advert) {
-        return advertRepository.filterByAdvert(advert);
-    }
-    public List<Adverts> filterByBuyer(AdvertStatus status, String buyer) {
-        return advertRepository.filterByBuyer(status, buyer);
-    }
+    Adverts updateAdvert(AdvertSaveRequest adverts);
 
-   // public Adverts getAdvertNo(Long advertNo){
-     //   return advertRepository.getAdvertNo(advertNo);
-    //}
+    boolean deleteAdvert(Long id);
 
+    Adverts getAdvert(Long id);
 
+    List<Adverts> filterByAdvert(AdvertStatus advert);
 
+    List<Adverts> filterByBuyer(AdvertStatus status, String buyer);
 }
--- src/main/java/com/example/EmlakBurada/services/UserService.java
+++ src/main/java/com/example/EmlakBurada/services/UserService.java
@@ -2,48 +2,17 @@ package com.example.EmlakBurada.services;
 
 import com.example.EmlakBurada.models.Users;
 import com.example.EmlakBurada.models.dtos.request.UserSaveRequest;
-import com.example.EmlakBurada.converter.UserConverter;
-import com.example.EmlakBurada.repositories.AdvertRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.EmlakBurada.repositories.UserRepository;
-@RequiredArgsConstructor
+
 @Service
-public class UserService {
-    @Autowired
-    UserRepository userRepository;
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-    public Users getById(Long id){
-        return userRepository.findById(id).get();
-    }
+public interface UserService {
+    Users getById(Long id);
 
-    public Users createUser(UserSaveRequest users) {
-        Users users1 = UserConverter.toUsers(users);
-        return userRepository.save(users1);
-    }
+    Users createUser(UserSaveRequest users);
 
-    public Users updateAdvert(UserSaveRequest users) {
-        Users users1 = UserConverter.toUsers(users);
-        return userRepository.save(users1);
-    }
+    Users updateAdvert(UserSaveRequest users);
 
-    public Users getUser(Long id) {
-        return userRepository.getById(id);
-    }
-    public boolean deleteUser(Long id){
-        userRepository.deleteById(id);
-        return true;
-    }
+    Users getUser(Long id);
 
-    public Users getByLogin(String email, String password) {
-        Users users = userRepository.findByEmailAndPassword(email,password);
-        if (users == null) {
-            return null;
-        } else {
-            return users;
-        }
-    }
+    Users getByLogin(String email, String password);
 }
