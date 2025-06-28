d32f9399d4dc7c774114b42254b4745ed85a492f
zeynepaltay <altay-zeynep@hotmail.com>
2025-06-28 18:37:33 +0300

    user repo optinal added

--- src/main/java/com/example/EmlakBurada/repositories/UserRepository.java
+++ src/main/java/com/example/EmlakBurada/repositories/UserRepository.java
@@ -3,9 +3,9 @@ package com.example.EmlakBurada.repositories;
 import com.example.EmlakBurada.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
-
-    Users findByEmailAndPassword(String email, String password);
+    Optional<Users> findByEmailAndPassword(String email, String password);
 }
