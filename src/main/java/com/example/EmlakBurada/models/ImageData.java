130203dae837735f7c3619b366b9dcf9c72dc08a
zeynepaltay <altay-zeynep@hotmail.com>
2025-07-08 21:09:38 +0300

    unnecessary parts are deleted

--- src/main/java/com/example/EmlakBurada/models/ImageData.java
+++ src/main/java/com/example/EmlakBurada/models/ImageData.java
@@ -1,6 +1,5 @@
 package com.example.EmlakBurada.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,10 +24,6 @@ public class ImageData {
     @Lob
     @Column(name = "imagedata", length = 1000)
     private byte[] imageData;
-
-    @ManyToOne
-    @JoinColumn(name = "adverts_id")
-    private Packets advertes;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "advert_id")
